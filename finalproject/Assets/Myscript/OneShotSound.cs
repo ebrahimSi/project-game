89d4a6b2b6f745a71497771b1686d22d0f0cf56c
ebrahimsi <hhooaa2013@gmail.com>
2019-10-29 22:18:40 +0300

    section 4
    sound

---
+++ finalproject/Assets/Myscript/OneShotSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneShotSound : MonoBehaviour
+{
+    [SerializeField] GameObject SoundSource;
+
+    // Start is called before the first frame update
+   void OnTriggerEnter (Collider other)
+   {
+
+    if(other.gameObject.CompareTag("Player"))
+    {
+        SoundSource.gameObject.SetActive(true);
+        Destroy(gameObject,7);
+    }
+   }
+}
