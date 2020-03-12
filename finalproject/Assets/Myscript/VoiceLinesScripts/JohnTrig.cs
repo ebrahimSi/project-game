b49f533aca303c4c5be80a9373e31e4fbafec21e
AliMahjari <43079544+AliMahjari@users.noreply.github.com>
2020-03-12 16:54:42 +0300

    voicelineTest

---
+++ finalproject/Assets/Myscript/VoiceLinesScripts/JohnTrig.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class JohnTrig : MonoBehaviour
+{
+   public GameObject ThePlayer;
+    
+  public  GameObject TextBox;
+
+
+   void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(ScenePlayer());
+    }
+
+   
+
+
+    IEnumerator ScenePlayer()
+    {
+        yield return new WaitForSeconds(1.5f);
+      
+        TextBox.GetComponent<Text>().text = "johnathan !!";
+        yield return new WaitForSeconds(1.5f);
+        TextBox.GetComponent<Text>().text = "johnathaaaaan !!";
+        yield return new WaitForSeconds(1.5f);
+        TextBox.GetComponent<Text>().text = "";
+         Destroy(TextBox);
+
+
+    }
+}
